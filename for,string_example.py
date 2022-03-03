5302e02b3f8b709b9dd5eb7bd8a247ccf0b08fa5
LvonHabsburg <lvonhabsburg.ieu2020@student.ie.edu>
2022-03-03 17:55:32 +0100

    Secon Session Examples

---
+++ for,string_example.py
@@ -0,0 +1,8 @@
+greeting = "hello, how are you?"
+for letter in greeting:
+    print(letter*2, end="")
+#end="" is so that the code is all on the same line
+print()
+
+print(greeting[2::2])
+#strings in steps of 2 starting at the 2nd position
